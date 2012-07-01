0d36c5937a56ada06d9d74116743ddaba0cafb23
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-01 16:19:22 -0400

    Fixed Html Layout
    Fixed Html Layout

--- Typeset.Web/App_Data/content/js/default/default.js
+++ Typeset.Web/App_Data/content/js/default/default.js
@@ -2,9 +2,8 @@
 $(document).ready(function () {
     //Apply twitter Bootstrap classes
     $('body').addClass('container');
-    $('body > header').addClass('span2');
-    $('body > footer').addClass('row-fluid span12');
-    $('#content').addClass('row-fluid');
-    $('#content > .posts').addClass('span8');
-    $('#content > article.post').addClass('span8');
+    $('body > header, body > #content, body > footer').wrapAll('<section class="row-fluid" />');
+    $('body > .row-fluid > header').addClass('span2');
+    $('body > .row-fluid > #content').addClass('span8');
+    $('body > .row-fluid > footer').addClass('span12');
 });
