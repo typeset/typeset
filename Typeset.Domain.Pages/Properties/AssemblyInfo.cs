e4dc7d93c4b5f32e9d72b51d28b1ff870f531ee7
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-06 18:25:28 -0400

    Deleted About, Pages, and Posts Directories
    Deleted Typeset.Domain.About Directory
    Deleted Typeset.Domain.Pages Directory
    Deleted Typeset.Domain.Posts Directory

--- Typeset.Domain.Pages/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Typeset.Domain.Pages")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: Guid("07a5e08c-28d0-4959-a2f6-fb99afef4ac0")]
--- Typeset.Domain.Posts/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Typeset.Domain.Post")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: Guid("915ae0bc-db4f-4f14-adc6-79540f37a98b")]
