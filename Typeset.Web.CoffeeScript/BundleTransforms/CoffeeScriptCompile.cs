2900b97fbc58783b9041e8091c06d0669b2b0e08
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-06 23:24:09 -0400

    Refactored CoffeeScriptCompile
    Refactored CoffeeScriptCompile to be more simple.

--- Typeset.Web.CoffeeScript/BundleTransforms/CoffeeScriptCompile.cs
+++ Typeset.Web.CoffeeScript/BundleTransforms/CoffeeScriptCompile.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 
@@ -10,19 +9,22 @@ namespace Typeset.Web.CoffeeScript.BundleTransforms
     {
         public void Process(BundleContext context, BundleResponse response)
         {
-            var coffeeScriptFiles = response.Files.Where(f => f.Extension.Equals(".coffee", StringComparison.OrdinalIgnoreCase));
-            foreach (var file in coffeeScriptFiles)
-            {
-                using (var reader = new StreamReader(file.FullName))
-                {
-                    var coffeeScript = reader.ReadToEnd();
-                    var compiledCoffeeScript = CoffeeScript.Compiler.Compile(coffeeScript);
-                    reader.Close();
-                    response.Content = response.Content.Replace(coffeeScript, compiledCoffeeScript);
-                }
-            }
+            response.Content = string.Empty;
             response.ContentType = "text/javascript";
             response.Cacheability = HttpCacheability.Public;
+
+            foreach (var file in response.Files)
+            {
+                var fileText = File.ReadAllText(file.FullName);
+                if (file.Extension.Equals(".coffee", StringComparison.OrdinalIgnoreCase))
+                {
+                    response.Content += CoffeeScript.Compiler.Compile(fileText);
+                }
+                else
+                {
+                    response.Content += fileText;
+                }
+            }
         }
     }
 }
