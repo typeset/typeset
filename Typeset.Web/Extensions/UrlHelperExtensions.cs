018d3aff666cd4cf779a37a9a8ae8223fb3ac959
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-07-12 22:49:34 -0400

    Implemented AppHarbor's Absolute UrlHelper
    Updated usage where needed.

--- Typeset.Web/Extensions/UrlHelperExtensions.cs
+++ Typeset.Web/Extensions/UrlHelperExtensions.cs
@@ -4,17 +4,60 @@ namespace System.Web.Mvc
 {
     public static class UrlHelperExtensions
     {
+        public static string ToPublicUrl(this UrlHelper urlHelper, Uri relativeUri)
+        {
+            var httpContext = urlHelper.RequestContext.HttpContext;
+
+            var uriBuilder = new UriBuilder
+            {
+                Host = httpContext.Request.Url.Host,
+                Path = "/",
+                Port = 80,
+                Scheme = "http",
+            };
+
+            if (httpContext.Request.IsLocal)
+            {
+                uriBuilder.Port = httpContext.Request.Url.Port;
+            }
+
+            return new Uri(uriBuilder.Uri, relativeUri).AbsoluteUri;
+        }
+
+        public static string ToPublicUrl(this UrlHelper urlHelper, string relativeUri)
+        {
+            var httpContext = urlHelper.RequestContext.HttpContext;
+
+            var uriBuilder = new UriBuilder
+            {
+                Host = httpContext.Request.Url.Host,
+                Path = "/",
+                Port = 80,
+                Scheme = "http",
+            };
+
+            if (httpContext.Request.IsLocal)
+            {
+                uriBuilder.Port = httpContext.Request.Url.Port;
+            }
+
+            return new Uri(uriBuilder.Uri, relativeUri).AbsoluteUri;
+        }
+
         public static string Content(this UrlHelper helper, string contentPath, bool absolute)
         {
+            var url = string.Empty;
+            
             if (!absolute)
             {
-                return helper.Content(contentPath);
+                url = helper.Content(contentPath);
             }
             else
             {
-                var request = helper.RequestContext.HttpContext.Request;
-                return string.Format("{0}://{1}/{2}", request.Url.Scheme, request.Url.Authority, helper.Content(contentPath).TrimStart('/'));
+                url = helper.ToPublicUrl(contentPath);
             }
+
+            return url;
         }
     }
 }
