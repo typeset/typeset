80e41db8bd0876b676001f4002ad4b792936067f
Bates Westmoreland <rbwestmoreland@gmail.com>
2012-08-12 23:13:00 -0400

    Version 0.3.0
    Version bump to 0.3.0

--- Typeset.Common/GlobalAssemblyInfo.cs
+++ Typeset.Common/GlobalAssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.2.2")]
-[assembly: AssemblyFileVersion("0.2.2")]
+[assembly: AssemblyVersion("0.3.0")]
+[assembly: AssemblyFileVersion("0.3.0")]
